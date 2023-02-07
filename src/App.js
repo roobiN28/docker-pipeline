b6d4c80c0d3dfec4cd454b50ea5739e0caceb53b
Robert Zuba <robert.szymon.zuba@gmail.com>
2023-02-07 20:55:25 +0100

    test workflows 11

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Do not learn React double time
         </a>
       </header>
     </div>
